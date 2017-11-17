895b990f6ecc0d64fbe3f08795d6e29a6832b814
Joseph Weidinger <josephweidinger@carfax.com>
2017-11-16 20:16:58 -0600

    closer to MidiConvertWrapper because of a goof

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-# MidiConvertWrapper
-Simple wrapper over MidiConvert project to use simply via command line
+# MidiConvertWrapperAdvanced
+The intention in this project is to make a simple file processor that turns midi files into JSON files that can be streamed in real time by python / c++, caressed in real-time to inputs and labels for my model.
 
-## To use
 
-The build artifacts are committed in the build/ to make it easy to copy and use if that's all you want.
-
-All the dependencies are bundled so all you need is node.
-
-To use, run `node MidiConvertWrapper.js [file] [destination]`
-
-The [destination] argument is optional. If it is not provided, a file will be created alongside the location of [file].
-
-## To Develop
-
-It's pretty simple -> clone project and get to work.
-
-Develop: `npm run start`
-
-Build: `npm run build`
-
-## TODO
-
-Write tests to make sure it throws the right errors?
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,46 @@
-import { ParsedPath, parse} from 'path'
-import { writeFileSync, existsSync } from 'fs'
+import { MIDI, parse as parseMidi } from 'midiconvert'
+import { ParsedPath, parse, join } from 'path'
+import { readFileSync, writeFileSync, existsSync } from 'fs'
 
-if (process.argv.length !== 3) {
+if (process.argv.length !== 3 && process.argv.length !== 4) {
     console.error(`
-        You must have exactly one argument: the json file you want to minimize.
+        You must pass in one argument that is the midi file you wish to process.
+        Optionally, you may pass in a second argument that is the json file you
+        wish to save the output as.
     `)
     process.exit(1)
 }
 
 const firstArg: string = process.argv[2]
+const secondArg: string = process.argv[3]
 
 const sourcePathObj: ParsedPath = parse(firstArg)
+const destinationPathObj: ParsedPath = parse(secondArg || firstArg)
 
-if (sourcePathObj.ext !== '.json' || !existsSync(firstArg)) {
-    console.error('First argument must be a .json file that really exists.')
+if (sourcePathObj.ext !== '.mid' || !existsSync(firstArg)) {
+    console.error('First argument must be a .mid file that really exists.')
     process.exit(1)
 }
 
+if (secondArg && (destinationPathObj.ext !== '.json' || !existsSync(destinationPathObj.dir))) {
+    console.error('Second argument must be a .json file in a folder that really exists.')
+    process.exit(1)
+}
+
+const finalDestination: string = secondArg || `${join(sourcePathObj.dir, sourcePathObj.name)}.json`
 
 try {
 
-    const json = JSON.parse(require('fs').readFileSync(firstArg, 'utf8'));
-    writeFileSync(firstArg, JSON.stringify(json), 'utf8')
+    const midiFile: any = readFileSync(firstArg, 'binary')
+    const parsedMidi: MIDI = parseMidi(midiFile)
+    const serializedJson: string = JSON.stringify(parsedMidi)
+
+    writeFileSync(finalDestination, serializedJson, 'utf8')
+
 
 } catch (error) {
 
-    console.error('There was an minifying this json file. Error immediately follows...')
+    console.error('There was an error transforming this midi file to JSON. Error immediately follows...')
     console.error(error)
     process.exit(1)
 
